54a14e1ee77248eeefabe50f427b0e64921c189b
jessicaebd <jessica.natalia23oct@gmail.com>
2022-03-21 09:39:00 +0700

    first commit

---
+++ index.php
@@ -0,0 +1,45 @@
+<?php
+
+echo "<h1>Hello World!</h1>";
+
+//* Tipe Data
+echo "<h2>Tipe Data</h2>";
+$text = "Text";
+$number = 1;
+$float = 1.2;
+$boolean = true;
+
+echo "<br>";
+echo $text;
+echo "<br>";
+echo $number;
+echo "<br>";
+echo $float;
+echo "<br>";
+echo $boolean;
+
+//* Operator
+echo "<h2>Operator</h2>";
+$a = 1;
+$b = 2;
+$c = "1";
+
+$d = $a + $b;
+echo $d;
+
+echo "<br>";
+var_dump($a == $c);
+
+echo "<br>";
+var_dump($a === $c);
+
+
+// * Date
+echo "<h2>Date</h2>";
+$today = date("d/m/Y");
+echo $today;
+
+echo "<br>";
+
+date_default_timezone_set('Asia/Jakarta');
+echo date('D, d-M-y h:i:s');
---
+++ prosesRegister.php
@@ -0,0 +1,14 @@
+<?php
+
+if (isset($_POST['Submit'])) {
+    $username = $_POST['username'];
+    $password = $_POST['password'];
+
+    $_SESSION['username'] = $username;
+    $_SESSION['password'] = $password;
+
+    echo "Username: $username <br>";
+    echo "Password: $password <br>";
+} else {
+    echo "Tidak ada data yang dikirim";
+}
